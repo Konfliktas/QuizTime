dfa13b3cc4397605d1163c15da83d1f261afc44c
Šarūnas <saras0605@gmail.com>
2017-06-23 07:44:19 +0300

    Update config.js

--- src/components/config.js
+++ src/components/config.js
@@ -1,11 +1,11 @@
 //Name of the admin to authorise who can manage the quiz questions
 const adminName = "adminML"
 //Number of questions Quiz consists of
-const quizSize = 5;
+const quizSize = 10;
 //Number of questions each Admin Page Section consists of
 const sectionOfQuestionsSize = 4;
 //Period of seconds to solve the quiz
-const quizTimer = 180;
+const quizTimer = 60;
 //Api to get random set of questions for Quiz
 const quizUrl = '/api/quiz';
 //Api to get/post solved Quizes
